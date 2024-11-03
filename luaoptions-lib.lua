1d257c345b6edfd8fa83c886e78158c6759a0409
Erik Nijenhuis <erik@xerdi.com>
2024-11-03 17:14:40 +0100

    Fix log file location determination (#7)

--- luaoptions-lib.lua
+++ luaoptions-lib.lua
@@ -223,8 +223,8 @@ function tex_engine:__call()
 --[[
     Defines the properties extracted from the first line of jobname.log.
 --]]
-    local f = io.open(tex.jobname..'.log')
-    if not f then return end
+    local f = io.open(kpse.find_file(tex.jobname..'.log'))
+    if not f then return {} end
     self.engine, self.engine_version, self.dist, self.dist_version, self.format, self.format_version =
         f:read():match(
             'This is ([^,]*), Version ([^%(]*) %(([^%)]*) ([^%)]*)%)%s+%(format=([^%)]*) ([^)]*)%)'
